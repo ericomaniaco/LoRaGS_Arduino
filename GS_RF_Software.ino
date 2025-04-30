7419ab8a9aa702515e97d67c153cf15cd96e27cb
ericomaniaco <ericmaldonadonado@gmail.com>
2025-04-30 14:18:44 +0200

    Update GS_RF_Software.ino

--- GS_RF_Software.ino
+++ GS_RF_Software.ino
@@ -59,16 +59,17 @@ typedef enum {
 //___________________________________________________________
 //  VARIABLES
 uint8_t txPacket[PACKET_SIZE];  // Payload final tras encoding
-uint8_t RxData[PACKET_SIZE];
-uint8_t rxPacket[PACKET_SIZE];  // Buffer de recepción
+uint8_t rxData[PACKET_SIZE];
 uint8_t tcPacket[PACKET_SIZE] = {0xC8,0x9D,0x00,0x00,0x00,0x00,0x03,0x83,0x1E,0x19,0xDC,0x63,0x53,0xC4}; // Cabecera fija para cada comando
 uint8_t encodedPacket[48];     // Resultado del interleave
-int16_t rssi, rxSize;
-int tcNumber=0;
-int sendData_TLC_sent=0;
+
 volatile bool loraIdle = true;
-volatile int i=0;
 static RadioEvents_t RadioEvents;
+
+String tcInput;
+int tcNumber=0;
+int16_t rssi, rxSize;
+int sendData_TLC_sent=0;
 //___________________________________________________________
 
 //___________________________________________________________
@@ -76,8 +77,8 @@ static RadioEvents_t RadioEvents;
 void setup();
 void loop();
 void SendTC(uint8_t TC);
-void OnReceive(int packetSize);//OnRxDone
-//OnTxDone
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr );
+void OnTxDone();
 void Interleave(uint8_t *input, int size);
 void Deinterleave(uint8_t *input, int size);
 void PrintHex(uint8_t b);
@@ -111,7 +112,7 @@ void loop() {
   if (Serial.available() > 0)
   {
     tcInput=Serial.readStringUntil('\n');
-    tcNumber=tcinput.toInt();
+    tcNumber=tcInput.toInt();
     printf("Test");
     SendTC(tcNumber);
     delay(1800);
@@ -120,7 +121,7 @@ void loop() {
   
   if(loraIdle)
   {
-  	turnOffRGB();
+  	//turnOffRGB();
     loraIdle = false;
     Radio.Rx(0);
   }
@@ -128,246 +129,259 @@ void loop() {
 
 //REVISADA
 void SendTC(uint8_t TC) {
-  // Coloca el byte de comando (TC) en la posición 2 del array tcpacket
-  tcpacket[2]=TC;
-  // Dependiendo del valor de TC, va rellenando el paquete con la información correspondiente
-  switch (TC) {
-      case PING:
-          printf("Executing: ping\n");
-          break;
-      case TRANSIT_TO_NM:
-          printf("Executing: transit to NM\n");
-          break;
-      case TRANSIT_TO_CM:
-          printf("Executing: transit to CM\n");
-          break;
-      case TRANSIT_TO_SSM:
-          printf("Executing: transit to SSM\n");
-          break;
-      case TRANSIT_TO_SM:
-          printf("Executing: transit to SM\n");
-          break;
-      case UPLOAD_ADCS_CALIBRATION:
-          printf("Executing: upload ADCS calibration TBD\n");
-          //TBD
-          break;
-      case UPLOAD_ADCS_TLE:
-          printf("Executing: upload ADCS TLE TBD\n");
-          //TBD
-          break;
-      case UPLOAD_COMMS_CONFIG:
-          printf("Executing: upload comms config TBD\n");
-          tcpacket[3]=128; // 128 == 868MHz , 255 == 915 MHz
-          tcpacket[4]=11; //Spreading factor
-          tcpacket[5]=1; //Coding rate
-          break;
-      case COMMS_UPLOAD_PARAMS:
-          printf("Executing: comms upload params\n");
-          tcpacket[3]=10; //Define rxtime in ms/100
-          tcpacket[4]=10; //Define sleeptime in ms/100
-          tcpacket[5]=64; //CADMODE --> 0 to 128==ON 128 to 255==OFF
-          tcpacket[6]=255; //Packet window for Payload data sending.
-          break;
-      case UPLOAD_UNIX_TIME:
-          printf("Executing: upload UNIX time TBD\n");
-          //TBD
-          break;
-      case UPLOAD_EPS_TH:
-          printf("Executing: upload EPS thresholds TBD\n");
-          //TBD
-          break;
-      case UPLOAD_PL_CONFIG:
-          printf("Executing: upload payload config TBD\n");
-          //TBD
-          break;
-      case DOWNLINK_CONFIG:
-          printf("Executing: downlink config TBD\n");
-          //TBD
-          break;
-      case EPS_HEATER_ENABLE:
-          printf("Executing: enable EPS heater TBD\n");
-          //TBD
-          break;
-      case EPS_HEATER_DISABLE:
-          printf("Executing: disable EPS heater TBD\n");
-          //TBD
-          break;
-      case POL_PAYLOAD_SHUT:
-          printf("Executing: shut down payload TBD\n");
-          break;
-      case POL_ADCS_SHUT:
-          printf("Executing: shut down ADCS TBD\n");
-          break;
-      case POL_BURNCOMMS_SHUT:
-          printf("Executing: shut down burn comms TBD\n");
-          break;
-      case POL_HEATER_SHUT:
-          printf("Executing: shut down heater TBD\n");
-          break;
-      case POL_PAYLOAD_ENABLE:
-          printf("Executing: enable payload TBD\n");
-          break;
-      case POL_ADCS_ENABLE:
-          printf("Executing: enable ADCS TBD\n");
-          break;
-      case POL_BURNCOMMS_ENABLE:
-          printf("Executing: enable burn comms TBD\n");
-          break;
-      case POL_HEATER_ENABLE:
-          printf("Executing: enable heater TBD\n");
-          break;
-      case CLEAR_PL_DATA:
-          printf("Executing: clear payload data TBD\n");
-          break;
-      case CLEAR_FLASH:
-          printf("Executing: clear flash memory TBD\n");
-          break;
-      case CLEAR_HT:
-          printf("Executing: clear housekeeping telemetry TBD\n");
-          break;
-      case COMMS_STOP_TX:
-          printf("Executing: stop comms transmission\n");
-          break;
-      case COMMS_RESUME_TX:
-          printf("Executing: resume comms transmission\n");
-          break;
-      case COMMS_IT_DOWNLINK:
-          printf("Executing: initiate comms downlink\n");
-          break;
-      case COMMS_HT_DOWNLINK:
-          printf("Executing: initiate housekeeping telemetry downlink TBD\n");
-          //TBD
-          break;
-      case PAYLOAD_SCHEDULE:
-          printf("Executing: schedule payload TBD\n");
-          //TBD
-          break;
-      case PAYLOAD_DEACTIVATE:
-          printf("Executing: deactivate payload TBD\n");
-          //TBD33
-          break;
-      case PAYLOAD_SEND_DATA:
-          printf("Executing: send payload data\n");
-          break;
-      case OBC_HARD_REBOOT:
-          printf("Executing: hard reboot of OBC TBD\n");
-          //TBD
-          break;
-      case OBC_SOFT_REBOOT:
-          printf("Executing: soft reboot of OBC TBRD\n");
-          //TBRD
-          break;
-      case OBC_PERIPH_REBOOT:
-          printf("Executing: reboot peripherals of OBC TBD\n");
-          //TBD
-          break;
-      case OBC_DEBUG_MODE:
-          printf("Executing: enable debug mode TBD\n");
-          //TBD
-          break;
-      default:
-          printf("Unknown command\n");
-          break;
-  }
-  // Reserva dinámicamente un buffer de bytes para el envío
-  uint8_t *TxData = (uint8_t *) malloc(PACKET_SIZE);
-  if (TxData == NULL) {
-       exit(EXIT_FAILURE);
-       serial.printf("Error en la linea 258");
-  }
-  // Copia el contenido de tcpacket (48 bytes) al buffer TxData
-  memcpy(TxData,tcpacket,PACKET_SIZE);
-  Serial.println();
-  // Aplica la función de interleaving al buffer TxData
-	interleave((uint8_t*) TxData, PACKET_SIZE);
-  // Copia el paquete entrelazado a Encoded_Packet
-	memcpy(Encoded_Packet,TxData,PACKET_SIZE);
-  // Libera la memoria dinámica ya que no la necesitamos más
-	free(TxData);
-  Serial.println();
-  // Envía el paquete final por el módulo de radio
-  Radio.Send(Encoded_Packet,sizeof(Encoded_Packet));
+    uint8_t *txData;
+    // Coloca el byte de comando (TC) en la posición 2 del array tcpacket
+    tcPacket[2]=TC;
+    // Dependiendo del valor de TC, va rellenando el paquete con la información correspondiente
+    switch (TC) {
+        case PING:
+            printf("Executing: ping\n");
+            break;
+        case TRANSIT_TO_NM:
+            printf("Executing: transit to NM\n");
+            break;
+        case TRANSIT_TO_CM:
+            printf("Executing: transit to CM\n");
+            break;
+        case TRANSIT_TO_SSM:
+            printf("Executing: transit to SSM\n");
+            break;
+        case TRANSIT_TO_SM:
+            printf("Executing: transit to SM\n");
+            break;
+        case UPLOAD_ADCS_CALIBRATION:
+            printf("Executing: upload ADCS calibration TBD\n");
+            //TBD
+            break;
+        case UPLOAD_ADCS_TLE:
+            printf("Executing: upload ADCS TLE TBD\n");
+            //TBD
+            break;
+        case UPLOAD_COMMS_CONFIG:
+            printf("Executing: upload comms config TBD\n");
+            tcPacket[3]=128; // 128 == 868MHz , 255 == 915 MHz
+            tcPacket[4]=11; //Spreading factor
+            tcPacket[5]=1; //Coding rate
+            break;
+        case COMMS_UPLOAD_PARAMS:
+            printf("Executing: comms upload params\n");
+            tcPacket[3]=10; //Define rxtime in ms/100
+            tcPacket[4]=10; //Define sleeptime in ms/100
+            tcPacket[5]=64; //CADMODE --> 0 to 128==ON 128 to 255==OFF
+            tcPacket[6]=255; //Packet window for Payload data sending.
+            break;
+        case UPLOAD_UNIX_TIME:
+            printf("Executing: upload UNIX time TBD\n");
+            //TBD
+            break;
+        case UPLOAD_EPS_TH:
+            printf("Executing: upload EPS thresholds TBD\n");
+            //TBD
+            break;
+        case UPLOAD_PL_CONFIG:
+            printf("Executing: upload payload config TBD\n");
+            //TBD
+            break;
+        case DOWNLINK_CONFIG:
+            printf("Executing: downlink config TBD\n");
+            //TBD
+            break;
+        case EPS_HEATER_ENABLE:
+            printf("Executing: enable EPS heater TBD\n");
+            //TBD
+            break;
+        case EPS_HEATER_DISABLE:
+            printf("Executing: disable EPS heater TBD\n");
+            //TBD
+            break;
+        case POL_PAYLOAD_SHUT:
+            printf("Executing: shut down payload TBD\n");
+            break;
+        case POL_ADCS_SHUT:
+            printf("Executing: shut down ADCS TBD\n");
+            break;
+        case POL_BURNCOMMS_SHUT:
+            printf("Executing: shut down burn comms TBD\n");
+            break;
+        case POL_HEATER_SHUT:
+            printf("Executing: shut down heater TBD\n");
+            break;
+        case POL_PAYLOAD_ENABLE:
+            printf("Executing: enable payload TBD\n");
+            break;
+        case POL_ADCS_ENABLE:
+            printf("Executing: enable ADCS TBD\n");
+            break;
+        case POL_BURNCOMMS_ENABLE:
+            printf("Executing: enable burn comms TBD\n");
+            break;
+        case POL_HEATER_ENABLE:
+            printf("Executing: enable heater TBD\n");
+            break;
+        case CLEAR_PL_DATA:
+            printf("Executing: clear payload data TBD\n");
+            break;
+        case CLEAR_FLASH:
+            printf("Executing: clear flash memory TBD\n");
+            break;
+        case CLEAR_HT:
+            printf("Executing: clear housekeeping telemetry TBD\n");
+            break;
+        case COMMS_STOP_TX:
+            printf("Executing: stop comms transmission\n");
+            break;
+        case COMMS_RESUME_TX:
+            printf("Executing: resume comms transmission\n");
+            break;
+        case COMMS_IT_DOWNLINK:
+            printf("Executing: initiate comms downlink\n");
+            break;
+        case COMMS_HT_DOWNLINK:
+            printf("Executing: initiate housekeeping telemetry downlink TBD\n");
+            //TBD
+            break;
+        case PAYLOAD_SCHEDULE:
+            printf("Executing: schedule payload TBD\n");
+            //TBD
+            break;
+        case PAYLOAD_DEACTIVATE:
+            printf("Executing: deactivate payload TBD\n");
+            //TBD33
+            break;
+        case PAYLOAD_SEND_DATA:
+            printf("Executing: send payload data\n");
+            break;
+        case OBC_HARD_REBOOT:
+            printf("Executing: hard reboot of OBC TBD\n");
+            //TBD
+            break;
+        case OBC_SOFT_REBOOT:
+            printf("Executing: soft reboot of OBC TBRD\n");
+            //TBRD
+            break;
+        case OBC_PERIPH_REBOOT:
+            printf("Executing: reboot peripherals of OBC TBD\n");
+            //TBD
+            break;
+        case OBC_DEBUG_MODE:
+            printf("Executing: enable debug mode TBD\n");
+            //TBD
+            break;
+        default:
+            printf("Unknown command\n");
+            break;
+    }
+    // Reserva dinámicamente un buffer de bytes para el envío
+    txData = (uint8_t *) malloc(PACKET_SIZE);
+    if (txData == NULL) {
+        exit(EXIT_FAILURE);
+        Serial.println("Error en la linea 258");
+    }
+    // Copia el contenido de tcpacket (48 bytes) al buffer txData
+    memcpy(txData,tcPacket,PACKET_SIZE);
+    Serial.println();
+    // Aplica la función de interleaving al buffer txData
+    interleave((uint8_t*) txData, PACKET_SIZE);
+    // Copia el paquete entrelazado a encodedPacket
+    memcpy(encodedPacket,txData,PACKET_SIZE);
+    // Libera la memoria dinámica ya que no la necesitamos más
+    free(txData);
+    Serial.println();
+    // Envía el paquete final por el módulo de radio
+    Radio.Send(encodedPacket,sizeof(encodedPacket));
 }
 
 void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr ) {
-    turnOnRGB(COLOR_RECEIVED,0);
+    int i;
+    uint8_t *rxPacket;
 
-  	memset(RxData,0,sizeof(RxData));
-    uint8_t *RxPacket =(uint8_t *) malloc(size);
-    if (RxPacket == NULL) {
+    //turnOnRGB(COLOR_RECEIVED,0);
+    //pots afegir printejar info per pantalla V3
+  	
+    memset(rxData,0,sizeof(rxData));
+    rxPacket =(uint8_t *) malloc(size);
+    if (rxPacket == NULL) {
         exit(EXIT_FAILURE);
     }
 
-    memcpy(RxPacket,payload,size);
+    memcpy(rxPacket,payload,size);
 
-    deinterleave((uint8_t*) RxPacket,size);
-    memcpy(RxData,RxPacket,size);
+    deinterleave((uint8_t*) rxPacket,size);
+    memcpy(rxData,rxPacket,size);
 
-    free(RxPacket);    
+    free(rxPacket);    
     Radio.Sleep();
     for(i=0; i<size; i++){
-      printHex(RxData[i]);
+      printHex(rxData[i]);
     }
     Serial.println();
-    lora_idle = true;
+    loraIdle = true;
 }
 
 void OnTxDone()
 {
-  lora_idle = true;;
+  loraIdle = true;;
 }
 
 void interleave(uint8_t *input, int size) {
-  // Check that the size is a multiple of 6.
+    int i , j;
+    int groupSize;
+    uint8_t *temp;
+
+    // Check that the size is a multiple of 6.
     if (size % 6 != 0) {
         return;
     }
 
-    int groupSize = size / 6;
+    groupSize = size / 6;
 
     // Allocate temporary array to hold the interleaved result.
-    uint8_t *temp =(uint8_t *) malloc(size * sizeof(uint8_t));
+    temp =(uint8_t *) malloc(size * sizeof(uint8_t));
     if (temp == NULL) {
         exit(EXIT_FAILURE);
     }
 
     // For each index within the groups,
     // pick one element from each of the 6 groups.
-    for (int i = 0; i < groupSize; i++) {
-        for (int j = 0; j < 6; j++) {
-            temp[i * 6 + j] = inputarr[j * groupSize + i];
+    for (i = 0; i < groupSize; i++) {
+        for (j = 0; j < 6; j++) {
+            temp[i * 6 + j] = input[j * groupSize + i];
         }
     }
 
     // Copy the interleaved elements back into the original array.
-    memcpy(inputarr, temp, size * sizeof(uint8_t));
+    memcpy(input, temp, size * sizeof(uint8_t));
 
     free(temp);
 }
 
 void deinterleave(uint8_t *input, int size) {
-  if (size % 6 != 0) {
-      return;
-  }
+    int i , j;
+    int groupSize;
+    uint8_t *temp;
 
-  int groupSize = size / 6;
-  uint8_t *temp =(uint8_t *) malloc(size * sizeof(uint8_t));
-  if (temp == NULL) {
-      exit(EXIT_FAILURE);
-  }
+    if (size % 6 != 0) {
+        return;
+    }
 
-  // Reconstruct the original groups.
-  // For each group index i and for each group j:
-  // The interleaved array holds the jth element of group j at position i*6 + j.
-  // We restore it to temp[ j * groupSize + i ].
-  for (int i = 0; i < groupSize; i++) {
-      for (int j = 0; j < 6; j++) {
-          temp[j * groupSize + i] = inputarr[i * 6 + j];
-      }
-  }
+    groupSize = size / 6;
+    temp =(uint8_t *) malloc(size * sizeof(uint8_t));
+    if (temp == NULL) {
+        exit(EXIT_FAILURE);
+    }
 
-  memcpy(inputarr, temp, size * sizeof(uint8_t));
-  free(temp);
+    // Reconstruct the original groups.
+    // For each group index i and for each group j:
+    // The interleaved array holds the jth element of group j at position i*6 + j.
+    // We restore it to temp[ j * groupSize + i ].
+    for (i = 0; i < groupSize; i++) {
+        for (j = 0; j < 6; j++) {
+            temp[j * groupSize + i] = input[i * 6 + j];
+        }
+    }
+
+    memcpy(input, temp, size * sizeof(uint8_t));
+    free(temp);
 }
 
 void printHex(uint8_t num) {
